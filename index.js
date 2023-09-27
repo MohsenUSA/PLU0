67c1c807b8bd64586ae8378b14e691ec300fcc6c
Mohsen <Mohsen.Seattle@gmail.com>
2023-09-27 04:41:52 -0700

    SEP 27 third commit!

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jsbarcode/3.11.3/JsBarcode.
 // Add an event listener to handle when the script is loaded
 script.onload = function() {
   // The library is now available for use
-  JsBarcode("#barcode1", "123456111");
-  JsBarcode("#barcode2", "123456222");
-  JsBarcode("#barcode3", "123456333");
-  JsBarcode("#barcode4", "123456444");
-  JsBarcode("#barcode5", "123456555");
-  JsBarcode("#barcode6", "123456666");
+  JsBarcode("#barcode1", "94041");
+  JsBarcode("#barcode2", "4044");
+  JsBarcode("#barcode3", "15038");
+  JsBarcode("#barcode4", "4225");
+  JsBarcode("#barcode5", "049000551051");
+  JsBarcode("#barcode6", "770118709026");
 
 };
 
