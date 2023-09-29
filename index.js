5505b6e74b2b17ea2ee727fb21bb85b64f7e9964
Mohsen <Mohsen.Seattle@gmail.com>
2023-09-29 01:35:44 -0700

    SEP 29 first commit adding barcode generator!

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var script = document.createElement('script');
 
 // Set the src attribute to the CDN URL
-script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jsbarcode/3.11.3/JsBarcode.all.min.js';
-
+// script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jsbarcode/3.11.3/JsBarcode.all.min.js';
+script.src = './JsBarcode.all.min.js';
 // Add an event listener to handle when the script is loaded
 script.onload = function() {
   // The library is now available for use
@@ -18,3 +18,4 @@ script.onload = function() {
 
 // Append the script element to the document
 document.head.appendChild(script);
+
